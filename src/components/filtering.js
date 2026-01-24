e475b2ec5feee0129de29e57f6e81410c24cf309
Ekaterina Dubinina <yekaterina.dubinina.2001@inbox.ru>
2026-01-24 18:38:50 -0400

    table

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,43 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
-
+const compare = createComparison(defaultRules);
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes).forEach((elementName) => {
+        if(elements[elementName]) {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName])
+                .map(name => {
+                    const option = document.createElement('option');
+                    option.value = name;
+                    option.textContent = name;
+                    return option;
+                })
+            );
+        }
+    });
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        console.log(data, state, action)
+        if(action && action.name === 'clear'){
+            const input = (action.parentElement.querySelector('input'))
+            input.value = ''
+            // const parent = action.target.parentElement;
+            // const input = parent ? parent.querySelector('input,select') : null;
 
+            // if(input){
+            //     input.value = '';
+
+            //     const fieldName = action.target.dataset.field;
+            //     if(fieldName && state) {
+            //         state[fieldName] = '';
+            //     }
+            // }
+        }
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
+        
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -3,6 +3,7 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    console.log(searchField)
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
